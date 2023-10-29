c0a470f79878a1a69cc00ef5754bbfdab082721a
Oussie <oussghrib@gmail.com>
2023-10-29 12:40:46 +0100

    hack to make useffect run once

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,16 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { getExercises } from '../services/exercises';
 function Exercises() {
 
     const [exercises, setExercises] = useState([]);
     const [offset, setOffset] = useState(0);
+    let testConst = exercises.length
+    useEffect(() => {
+        if (testConst < 10) {
+            testConst = handleLoadMore()
+        }
+       /*  window.addEventListener("load", scrollHandler, true);
+        return () => {
+          window.removeEventListener("load", scrollHandler, true);
+        }; */
+
+      }, [testConst]);
 
     async function handleLoadMore() {
         setOffset(prev => prev + 10);
         const exercises = await getExercises(offset)
 
         setExercises(prev => [...prev, ...exercises]); // Update exercises state
+
+        return exercises.length
     }
+    const inputRef = useRef();
+    const scrollHandler = () => {
+        console.log(inputRef.current.getBoundingClientRect());
+    } 
 
     return (
         <>
@@ -23,7 +40,8 @@ function Exercises() {
                 </div>
             ))}
 
-            <button onClick={handleLoadMore}>Load More</button>
+            <button ref={inputRef} onClick={handleLoadMore}>
+                Load More</button>
         </>
     )
 }
